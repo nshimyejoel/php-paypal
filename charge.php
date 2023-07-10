effc2b6fd70e38926433b6c1b29538d6cce117b2
nshimyejoel <nshimyejoel99@gmail.com>
2023-07-10 14:26:46 +0200

    Implementing PayPal integration using the powerful cURL method

--- charge.php
+++ charge.php
@@ -2,22 +2,96 @@
 require_once 'config.php';
 
 if (isset($_POST['submit'])) {
+    $amount = $_POST['amount'];
 
-    try {
-        $response = $gateway->purchase(array(
-            'amount' => $_POST['amount'],
-            'currency' => PAYPAL_CURRENCY,
-            'returnUrl' => PAYPAL_RETURN_URL,
-            'cancelUrl' => PAYPAL_CANCEL_URL,
-        ))->send();
+    
+    $authUrl = 'https://api.sandbox.paypal.com/v1/oauth2/token';
+    $clientId = urlencode(CLIENT_ID);
+    $clientSecret = urlencode(CLIENT_SECRET);
 
-        if ($response->isRedirect()) {
-            $response->redirect(); // this will automatically forward the customer
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $authUrl);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_USERPWD, $clientId . ':' . $clientSecret);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, 'grant_type=client_credentials');
+
+    $headers = array(
+        'Accept: application/json',
+        'Accept-Language: en_US',
+    );
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $authResponse = curl_exec($ch);
+    $authData = json_decode($authResponse, true);
+
+    if (isset($authData['access_token'])) {
+        $accessToken = $authData['access_token'];
+
+        
+        $createPaymentUrl = 'https://api.sandbox.paypal.com/v1/payments/payment';
+        $returnUrl = PAYPAL_RETURN_URL;
+        $cancelUrl = PAYPAL_CANCEL_URL;
+
+        $paymentData = array(
+            'intent' => 'sale',
+            'payer' => array('payment_method' => 'paypal'),
+            'transactions' => array(
+                array(
+                    'amount' => array(
+                        'total' => $amount,
+                        'currency' => PAYPAL_CURRENCY
+                    )
+                )
+            ),
+            'redirect_urls' => array(
+                'return_url' => $returnUrl,
+                'cancel_url' => $cancelUrl
+            )
+        );
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $createPaymentUrl);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Authorization: Bearer '. $accessToken
+        ));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($paymentData));
+
+        $paymentResponse = curl_exec($ch);
+        $paymentData = json_decode($paymentResponse, true);
+
+        if (isset($paymentData['id'])) {
+            $paymentId = $paymentData['id'];
+
+            
+            $approvalUrl = null;
+            foreach ($paymentData['links'] as $link) {
+                if ($link['rel'] === 'approval_url') {
+                    $approvalUrl = $link['href'];
+                    break;
+                }
+            }
+
+            if ($approvalUrl) {
+                header("Location: $approvalUrl");
+                exit;
+            } else {
+                echo "Failed to obtain PayPal approval URL.";
+            }
+        } elseif (isset($paymentData['error']) && isset($paymentData['error_description'])) {
+            echo "Payment creation failed: " . $paymentData['error_description'];
         } else {
-            // not successful
-            echo $response->getMessage();
+            echo "Payment creation failed. Error Response: ";
+            var_dump($paymentData);
         }
-    } catch(Exception $e) {
-        echo $e->getMessage();
+    } elseif (isset($authData['error']) && isset($authData['error_description'])) {
+        echo "Failed to obtain PayPal access token: " . $authData['error_description'];
+    } else {
+        echo "Failed to obtain PayPal access token. Error Response: ";
+        var_dump($authData);
     }
 }
+?>
--- config.php
+++ config.php
@@ -1,17 +1,11 @@
 <?php
-require_once "vendor/autoload.php";
 
-use Omnipay\Omnipay;
 
-define('CLIENT_ID', 'your client_id');
-define('CLIENT_SECRET', 'your client_secret');
+define('CLIENT_ID', 'AeQpypMHiyjhYOU98S1CLuwaLwfWv1J9H7leCqgu4RbqDaNk9lpiRWwnyX4wKZmRFIdm59oCzUXgn0mV');
+define('CLIENT_SECRET', 'EMOem7RJkXPMv8MPKFteo6KiXBVZ64M_5FxA8B85xU8yM3HXX4T0HHUYQU0qA-je3p6LAepNqQieHYT5');
 
 define('PAYPAL_RETURN_URL', 'http://localhost/paypal/success.php');
 define('PAYPAL_CANCEL_URL', 'http://localhost/paypal/cancel.php');
 define('PAYPAL_CURRENCY', 'USD'); // set your currency here
 
 
-$gateway = Omnipay::create('PayPal_Rest');
-$gateway->setClientId(CLIENT_ID);
-$gateway->setSecret(CLIENT_SECRET);
-$gateway->setTestMode(true); //set it to 'false' when go live
