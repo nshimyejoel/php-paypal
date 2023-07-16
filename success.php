4839a68e401f96dd108561e9589cb3f336d5a0b6
nshimyejoel <nshimyejoel99@gmail.com>
2023-07-16 12:15:21 +0200

    Refactor the method for displaying the success message with the transaction ID

--- success.php
+++ success.php
@@ -1,24 +1,9 @@
 <?php
 require_once 'config.php';
  
-// Once the transaction has been approved, we need to complete it.
 if (array_key_exists('paymentId', $_GET) && array_key_exists('PayerID', $_GET)) {
-    $transaction = $gateway->completePurchase(array(
-        'payer_id'             => $_GET['PayerID'],
-        'transactionReference' => $_GET['paymentId'],
-    ));
-    $response = $transaction->send();
- 
-    if ($response->isSuccessful()) {
-        // The customer has successfully paid.
-        $arr_body = $response->getData();
- 
-        $payment_id = $arr_body['id'];
-        $payer_id = $arr_body['payer']['payer_info']['payer_id'];
-        $payer_email = $arr_body['payer']['payer_info']['email'];
-        $amount = $arr_body['transactions'][0]['amount']['total'];
-        $currency = PAYPAL_CURRENCY;
-        $payment_status = $arr_body['state'];
+    $payment_id = $_GET['paymentId'];
+    $payer_id = $_GET['PayerID'];
  
  ?>
 
@@ -65,9 +50,6 @@ if (array_key_exists('paymentId', $_GET) && array_key_exists('PayerID', $_GET)) 
 
  <?php
     
-    } else {
-        echo $response->getMessage();
-    }
 } else {
     echo 'Transaction is declined';
 }
