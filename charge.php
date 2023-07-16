03c9ba6ff40fa3591eb4dd3e0bffcbb51b1bbc2f
nshimyejoel <nshimyejoel99@gmail.com>
2023-07-16 12:13:21 +0200

    Refactor the implementation of PayPal integration by replacing cURL with Guzzle

--- charge.php
+++ charge.php
@@ -1,37 +1,35 @@
 <?php
+require_once 'vendor/autoload.php';
 require_once 'config.php';
+
+use GuzzleHttp\Client;
 
 if (isset($_POST['submit'])) {
     $amount = $_POST['amount'];
 
-
     $auth_url = 'https://api.sandbox.paypal.com/v1/oauth2/token';
-    $client_id = urlencode(CLIENT_ID);
-    $client_secret = urlencode(CLIENT_SECRET);
+    $client_id = CLIENT_ID;
+    $client_secret = CLIENT_SECRET;
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $auth_url);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_USERPWD, $client_id . ':' . $client_secret);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, 'grant_type=client_credentials');
+    $client = new Client();
 
-    $headers = [
-        'Accept: application/json',
-        'Accept-Language: en_US',
-    ];
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    $auth_response = $client->request('post', $auth_url, [
+        'auth' => [$client_id, $client_secret],
+        'form_params' => ['grant_type' => 'client_credentials'],
+        'headers' => [
+            'Accept' => 'application/json',
+            'Accept-Language' => 'en_US',
+        ],
+    ]);
 
-    $auth_response = curl_exec($ch);
-    $auth_data = json_decode($auth_response, true);
+    $auth_data = json_decode($auth_response->getBody(), true);
 
     if (isset($auth_data['access_token'])) {
         $access_token = $auth_data['access_token'];
 
-
         $create_payment_url = 'https://api.sandbox.paypal.com/v1/payments/payment';
         $return_url = PAYPAL_RETURN_URL;
-        $cancel_url  = PAYPAL_CANCEL_URL;
+        $cancel_url = PAYPAL_CANCEL_URL;
 
         $payment_data = [
             'intent' => 'sale',
@@ -46,26 +44,22 @@ if (isset($_POST['submit'])) {
             ],
             'redirect_urls' => [
                 'return_url' => $return_url,
-                'cancel_url' => $cancel_url 
+                'cancel_url' => $cancel_url
             ]
         ];
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $create_payment_url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'Content-Type: application/json',
-            'Authorization: Bearer ' . $access_token
+        $payment_response = $client->request('post', $create_payment_url, [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Bearer ' . $access_token
+            ],
+            'json' => $payment_data,
         ]);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payment_data));
 
-        $payment_response = curl_exec($ch);
-        $payment_data = json_decode($payment_response, true);
+        $payment_data = json_decode($payment_response->getBody(), true);
 
         if (isset($payment_data['id'])) {
             $payment_id = $payment_data['id'];
-
 
             $approval_url = null;
             foreach ($payment_data['links'] as $link) {
@@ -85,13 +79,11 @@ if (isset($_POST['submit'])) {
             echo "Payment creation failed: " . $payment_data['error_description'];
         } else {
             echo "Payment creation failed. Error Response: ";
-
         }
     } elseif (isset($auth_data['error']) && isset($auth_data['error_description'])) {
         echo "Failed to obtain PayPal access token: " . $auth_data['error_description'];
     } else {
         echo "Failed to obtain PayPal access token. Error Response: ";
-
     }
 }
 ?>
