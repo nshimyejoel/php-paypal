52e50e5e525bb4769811fa36583808e65c9c08bc
nshimyejoel <nshimyejoel99@gmail.com>
2023-07-24 12:49:51 +0200

    initial commit

---
+++ googleLogin/config.php
@@ -0,0 +1,8 @@
+<?php
+
+
+const CLIENT_ID = '769078040402-0hlqtsf1pim43srkrn6o43konu4hmkpu.apps.googleusercontent.com';
+const CLIENT_SECRET = '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';
+const REDIRECT_URL = 'http://localhost/paypal/googleLogin/';
+
+
