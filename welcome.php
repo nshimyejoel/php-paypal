58e16846dbed1e01862bdc886372e3b6868f85ec
nshimyejoel <nshimyejoel99@gmail.com>
2023-07-25 10:41:56 +0200

    Refactor HTML to utilize defined CSS and remove external libraries

--- welcome.php
+++ welcome.php
@@ -5,26 +5,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Welcome</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <style>
-    body {
-      display: flex;
-      flex-direction: column;
-      min-height: 100vh;
-    }
-
-    .content {
-      flex: 1;
-    }
-  </style>
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
-
-  <!-- Header -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar">
     <a class="navbar-brand" href="#">Payment</a>
-    <ul class="navbar-nav ml-auto">
+    <ul class="navbar-nav">
       <li class="nav-item">
         <a class="nav-link" href="home">Home</a>
       </li>
@@ -34,7 +21,6 @@
     </ul>
   </nav>
 
-  <!-- Content -->
   <div class="content container mt-5">
     <div class="row justify-content-center">
       <div class="col-md-6">
@@ -47,15 +33,11 @@
       </div>
     </div>
   </div>
+  
 
-  <!-- Footer -->
-  <footer class="bg-dark text-white text-center py-3">
+  <footer class="footer">
     &copy; 2023 Nshimye Joel. All rights reserved.
   </footer>
-
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 </body>
 
